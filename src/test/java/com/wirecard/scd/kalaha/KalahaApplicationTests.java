366bb0a01060dd7d542058db4fe3162fc9a0912d
harho <harho@centrumsk>
2017-05-18 11:12:18 +0200

    Pavol: Pit test failed.

--- src/test/java/com/wirecard/scd/kalaha/KalahaApplicationTests.java
+++ src/test/java/com/wirecard/scd/kalaha/KalahaApplicationTests.java
@@ -1,20 +1,27 @@
 package com.wirecard.scd.kalaha;
 
+import com.wirecard.scd.kalaha.domain.Board;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//SpringBootTest
 public class KalahaApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
 
 	@Test
 	public void hasBoard() {
+		Board board = createBoard();
+		Assert.assertNotNull(board);
+
+	}
+	@Test
+	public void hasPit() {
+		Pit pit = null;
+		Assert.assertNotNull(pit);
 
 	}
 
@@ -23,11 +30,11 @@ public class KalahaApplicationTests {
 		return board;
 	}
 
-	public class Board{
-
-
+	public class Pit {
 
 	}
+
+
 
 
 
